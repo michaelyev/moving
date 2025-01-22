ab6d0fc13b3501ee4178121ae66600208df0fb99
michaelyev <michaelyevt@gmail.com>
2025-01-21 22:33:22 -0800

    thank you

---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <section className='container w-full '>
+      {" "}
+      <h1 className="text-center">
+        Thanks! We will be in touch with you shortly!
+      </h1>
+    </section>
+  );
+}
+
+export default page
